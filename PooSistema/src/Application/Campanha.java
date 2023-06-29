3cecc80adc9e6a55d50e22cc54032bafcea9dccb
CiceroOliver <137697572+CiceroOliver@users.noreply.github.com>
2023-06-28 21:35:16 -0300

    Update Campanha.java

--- PooSistema/src/Application/Campanha.java
+++ PooSistema/src/Application/Campanha.java
@@ -19,57 +19,57 @@ public class Campanha {
 		this.saldo = 0;
 		this.golsMarcados = 0;
 		this.golsSofridos = 0;
-		//this.confrontosDiretos = new Vector <Clubes>;
+		this.confrontosDiretos = new Vector<>(10);
 		//implementar com string talvez
 	}
 	public int getPontos() {
 		return pontos;
 	}
 	public void setPontos(int pontos) {
-		this.pontos = pontos;
+		this.pontos = this.pontos + pontos;
 	}
 	public int getNumeroDeVitorias() {
 		return numeroDeVitorias;
 	}
 	public void setNumeroDeVitorias(int numeroDeVitorias) {
-		this.numeroDeVitorias = numeroDeVitorias;
+		this.numeroDeVitorias = this.numeroDeVitorias + numeroDeVitorias;
 	}
 	public int getNumeroDeEmpates() {
 		return numeroDeEmpates;
 	}
 	public void setNumeroDeEmpates(int numeroDeEmpates) {
-		this.numeroDeEmpates = numeroDeEmpates;
+		this.numeroDeEmpates = this.numeroDeEmpates + numeroDeEmpates;
 	}
 	public int getNumeroDeDerrotas() {
 		return numeroDeDerrotas;
 	}
 	public void setNumeroDeDerrotas(int numeroDeDerrotas) {
-		this.numeroDeDerrotas = numeroDeDerrotas;
+		this.numeroDeDerrotas = this.numeroDeDerrotas + numeroDeDerrotas;
 	}
 	public int getSaldo() {
 		return saldo;
 	}
 	public void setSaldo(int saldo) {
-		this.saldo = saldo;
+		this.saldo = this.getGolsMarcados() - this.getGolsSofridos();
 		//Aqui é necessario atualizar a partir dos golsofridos/marcados;
 	}
 	public int getGolsMarcados() {
 		return golsMarcados;
 	}
 	public void setGolsMarcados(int golsMarcados) {
-		this.golsMarcados = golsMarcados;
+		this.golsMarcados = this.golsMarcados + golsMarcados;
 	}
 	public int getGolsSofridos() {
 		return golsSofridos;
 	}
 	public void setGolsSofridos(int golsSofridos) {
-		this.golsSofridos = golsSofridos;
+		this.golsSofridos = this.golsSofridos + golsSofridos;
 	}
 	public Clube[] getConfrontosDiretos() {
-		return confrontosDiretos;
+		return confrontosDiretos.toArray(new Clube[confrontosDiretos.size()]);
 	}
 	public void setConfrontosDiretos(Clube confrontosDiretos) {
-		this.confrontosDiretos = confrontosDiretos;
+		this.confrontosDiretos.add(confrontosDiretos);
 	} 
 
     //mete os get e set aí
