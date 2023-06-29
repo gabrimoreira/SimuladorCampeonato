8dee77ccfde734a14a96845b1709fd4ac276bd1d
CiceroOliver <137697572+CiceroOliver@users.noreply.github.com>
2023-06-28 21:14:16 -0300

    Update Campanha.java

--- PooSistema/src/Application/Campanha.java
+++ PooSistema/src/Application/Campanha.java
@@ -2,16 +2,26 @@ package Application;
 
 public class Campanha {
     //atualizados a cada partida instanciada.
-    private int pontos;
-    private int numeroDeVitorias;
-    private int numeroDeEmpates;
-    private int numeroDeDerrotas;
-    private int saldo;
-    private int golsMarcados;
-    private int golsSofridos;
-    private Vector<Clubes> confrontosDiretos;
+	private int pontos;
+	private int numeroDeVitorias;
+	private int numeroDeEmpates;
+	private int numeroDeDerrotas;
+	private int saldo;
+	private int golsMarcados;
+	private int golsSofridos;
+	private Vector<Clubes> confrontosDiretos;
  // guarda o confronto direto com cada time adversario, caso o usuario queira consultar os resultados das partidas ou em caso de necessidade como criterio de desempate.
-
+	public Campanha(){
+		this.pontos = 0;
+		this.numeroDeVitorias = 0;
+		this.numeroDeEmpates = 0;
+		this.numeroDeDerrotas = 0;
+		this.saldo = 0;
+		this.golsMarcados = 0;
+		this.golsSofridos = 0;
+		//this.confrontosDiretos = new Vector <Clubes>;
+		//implementar com string talvez
+	}
 	public int getPontos() {
 		return pontos;
 	}
@@ -41,6 +51,7 @@ public class Campanha {
 	}
 	public void setSaldo(int saldo) {
 		this.saldo = saldo;
+		//Aqui é necessario atualizar a partir dos golsofridos/marcados;
 	}
 	public int getGolsMarcados() {
 		return golsMarcados;
